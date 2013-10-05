ffd4dfb3be8117e0ace0be2812e768e576c7489e
Alexandre St-Onge <alexandre.st-onge@polymtl.ca>
2013-10-05 14:22:07 -0400

    Ajout d'un mouvement simple de la palette

--- Breakout/Breakout/GameObjects/Palette.cs
+++ Breakout/Breakout/GameObjects/Palette.cs
@@ -18,12 +18,14 @@ namespace Breakout
         public Vector2 position;
         private Texture2D sprite;
         private Rectangle screenBound;
+        public int speedX;
 
         public Palette(Texture2D sprite, Rectangle screenBound)
         {
             position = new Vector2();
             this.sprite = sprite;
             this.screenBound = screenBound;
+            speedX = 5;
             setInitPosition();
         }
 
@@ -31,6 +33,18 @@ namespace Breakout
         {
             position.X = screenBound.Width / 2 - 50;
             position.Y = screenBound.Height - 50;
+        }
+
+        public void Update(KeyboardState state)
+        {
+            if (state.IsKeyDown(Keys.Left))
+            {
+                position.X -= speedX;
+            }
+            else if(state.IsKeyDown(Keys.Right))
+            {
+                position.X += speedX;
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch)
