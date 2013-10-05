fc9a1399cd208c6181a25bce396efd33582bb054
Alexandre St-Onge <alexandre.st-onge@polymtl.ca>
2013-10-05 01:16:09 -0400

    Création de la classe Palette.cs

---
+++ Breakout/Breakout/GameObjects/Palette.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Breakout
+{
+    class Palette
+    {
+        public Vector2 position;
+        private Texture2D sprite;
+        private Rectangle screenBound;
+
+        public Palette(Texture2D sprite, Rectangle screenBound)
+        {
+            position = new Vector2();
+            this.sprite = sprite;
+            this.screenBound = screenBound;
+            setInitPosition();
+        }
+
+        private void setInitPosition()
+        {
+            position.X = screenBound.Width / 2 - 50;
+            position.Y = screenBound.Height - 50;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, position, Color.White);
+        }
+    }
+}
