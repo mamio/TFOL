e1d6b784fec8618ea8bd426599d0ea6b0558abfc
Marie-France Miousse <mfmiousse@gmail.com>
2013-10-31 11:04:15 -0400

    menu

---
+++ Breakout/Breakout/GameObjects/BoutonExit.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+using System;
+
+namespace Breakout
+{
+    class BoutonExit
+    {
+        private Texture2D exitButton;
+        private Vector2 exitButtonPosition;
+        private Rectangle screenBound;
+
+        public BoutonExit(Texture2D sprite, Rectangle screenBound)
+        {
+            this.exitButton = sprite;
+            this.screenBound = screenBound;
+            setPosition();
+        }
+
+        private void setPosition()
+        {
+            exitButtonPosition.X = (screenBound.Width / 2) - (exitButton.Width / 2);
+            exitButtonPosition.Y = screenBound.Height - 200;
+        }
+
+        public float getPositionX()
+        {
+            return exitButtonPosition.X;
+        }
+
+        public float getPositionY()
+        {
+            return exitButtonPosition.Y;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(exitButton, exitButtonPosition, Color.White);
+        }
+    }
+}
