0502ad263f4c8328d6bc79077f37ae4ccd85d024
Marie-France Miousse <mfmiousse@gmail.com>
2013-10-31 12:17:29 -0400

    pause quand appuie sur enter et resume avec bouton

---
+++ Breakout/Breakout/GameObjects/Resume.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+using System;
+
+
+namespace Breakout
+{
+    class Resume
+    {
+        private Texture2D resumeButton;
+        private Vector2 resumeButtonPosition;
+        private Rectangle screenBound;
+
+        public Resume(Texture2D sprite, Rectangle screenBound)
+        {
+            this.resumeButton = sprite;
+            this.screenBound = screenBound;
+            setPosition();
+        }
+
+        private void setPosition()
+        {
+            resumeButtonPosition.X = (screenBound.Width / 2) - (resumeButton.Width / 2);
+            resumeButtonPosition.Y = (screenBound.Height / 2) - (resumeButton.Height / 2);
+        }
+
+        public float getPositionX()
+        {
+            return resumeButtonPosition.X;
+        }
+
+        public float getPositionY()
+        {
+            return resumeButtonPosition.Y;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(resumeButton, resumeButtonPosition, Color.White);
+        }
+    }
+}
